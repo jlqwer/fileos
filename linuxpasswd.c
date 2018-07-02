031138748c0fd06a5f9f57694395361a9d186930
jlqwer <jlqwer@126.com>
2018-07-02 12:18:39 +0800

    重写getch（仅linux下调用）

---
+++ linuxpasswd.c
@@ -0,0 +1,26 @@
+#ifndef WIN32
+#include<stdio.h>
+#include<termios.h>
+#include<unistd.h>
+#include<assert.h>
+#include<string.h>
+int getch()
+{
+    int c=0;
+    struct termios org_opts, new_opts;
+    int res=0;
+    //-----  store old settings -----------
+    res=tcgetattr(STDIN_FILENO, &org_opts);
+    assert(res==0);
+    //---- set new terminal parms --------
+    memcpy(&new_opts, &org_opts, sizeof(new_opts));
+    new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
+    c=getchar();
+    //------  restore old settings ---------
+    res=tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
+    assert(res==0);
+    return c;
+
+}
+#endif
